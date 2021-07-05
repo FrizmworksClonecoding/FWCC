d83689c60eb29d5422bc14644484221467ac9362
rihee1214 <rihee1214@gmail.com>
2021-07-05 22:29:27 +0900

    adminController 수정
    뒤죽박죽으로 섞여있어 순서 수정

--- src/main/java/com/fw/s1/admin/AdminController.java
+++ src/main/java/com/fw/s1/admin/AdminController.java
@@ -59,100 +59,66 @@ public class AdminController {
 	@GetMapping("adminHome")
 	public void adminHome() { }
 	
-	//쿠폰 종류를 입력하는 페이지로 이동
-	@GetMapping("setCouponsp")
-	public void setCouponsp()throws Exception {	}
+	@GetMapping("orderlistUpdate")
+	public void orderlistUpdate()throws Exception{	}
 	
-	//쿠폰 종류 입력후 저장을 위함
-	@PostMapping("setCouponsp")
-	public String setCouponsp(CouponspVO couponspVO)throws Exception{
-		couponService.setCouponsp(couponspVO);
-		return "redirect:/admin/adminHome";
-	}
-	
-	//쿠폰 목록을 보여주는 페이지
-	@GetMapping("transmitCoupon")
-	public void transmitCoupon(Model model)throws Exception {
-		model.addAttribute("cuspList", couponService.getCouponspList());
-	}
-	
-	//선택한 쿠폰을 보낼 페이지로 이동
-	@GetMapping("selectedCoupon")
-	public void selectedCoupon(CouponspVO couponspVO, Model model)throws Exception{
-		model.addAttribute("item", couponService.getCouponsp(couponspVO));
-	}
-	
-	//쿠폰을 모든 사람들에게 전송
 	@ResponseBody
-	@GetMapping("couponforall")
-	public Long couponforall(Long cuSpNum, Integer period)throws Exception{
-		Calendar calendar = Calendar.getInstance();
-		java.sql.Date date1 = new java.sql.Date(calendar.getTimeInMillis());
-		calendar.add(Calendar.MONTH, period);
-		java.sql.Date date2 = new java.sql.Date(calendar.getTimeInMillis());
-		if(date1.after(date2)||date1.equals(date2)) {
-			return 0L;
+	@PostMapping("orderlistUpdate")
+	@Transactional(rollbackFor = Exception.class)
+	public Long orderlistUpdate(OrderlistVO orderlistVO)throws Exception{
+		java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+		Long temp = orderlistVO.getOrderState();
+		if(orderlistVO.getOrderState()==4) {
+			orderlistVO = orderService.selectedOrder(orderlistVO);
+			if(!date.toString().equals(orderlistVO.getOrderDate().toString())) {
+				List<PurchaseVO> list = purchaseService.getPurAdmin(orderlistVO);
+				if(adminService.cancledOrder(list) == 0) {
+					throw new Exception();
+				}
+			}
+		}else {
+			orderlistVO = orderService.selectedOrder(orderlistVO);
+			if(orderlistVO.getOrderState()==4) {
+				List<PurchaseVO> list = purchaseService.getPurAdmin(orderlistVO);
+				if(adminService.reCancledOrder(list) == 0) {
+					throw new Exception();
+				}
+			}
 		}
-		CouponVO couponVO = new CouponVO();
-		couponVO.setCuSpNum(cuSpNum);
-		couponVO.setPubDate(date1);
-		couponVO.setExDate(date2);
-
-		return couponService.couponForAll(couponVO);
+		orderlistVO.setOrderState(temp);
+		return orderService.orderlistUpdate(orderlistVO);
 	}
 	
-	//쿠폰을 입력한 username들에게 보냄
 	@ResponseBody
-	@PostMapping("couponServeral")
-	public Long couponServeral(String[] usernames, Long cuSpNum, Integer period)throws Exception{
-		List<CouponVO> list = new ArrayList<>();
-		Calendar calendar = Calendar.getInstance();
-		java.sql.Date date1 = new java.sql.Date(calendar.getTimeInMillis());
-		calendar.add(Calendar.MONTH, period);
-		java.sql.Date date2 = new java.sql.Date(calendar.getTimeInMillis());
-		if(date1.after(date2)||date1.equals(date2)) {
-			return 0L;
+	@PostMapping("orderlistsUpdate")
+	public Long orderlistsUpdate(String[] orderNums)throws Exception{
+		List<OrderlistVO> list = new ArrayList<OrderlistVO>();
+		for(String temp : orderNums) {
+			OrderlistVO orderlistVO = new OrderlistVO();
+			orderlistVO.setOrderNum(temp);
+			list.add(orderlistVO);
 		}
-		for(String username : usernames) {
-			CouponVO couponVO = new CouponVO();
-			couponVO.setUsername(username);
-			couponVO.setCuSpNum(cuSpNum);
-			couponVO.setPubDate(date1);
-			couponVO.setExDate(date2);
-			list.add(couponVO);
-		}
-		return couponService.couponForSelected(list);
+		return orderService.orderlistsUpdate(list);
 	}
 	
-	//마일리지 전송하는 페이지
-	@GetMapping("transmitMile")
-	public void transmitMile() { }
-	
-	//모든 사람들에게 마일리지를 전송
 	@ResponseBody
-	@GetMapping("transAllMile")
-	public Long transAllMile(Long mileage, String contents)throws Exception {
-		MileageVO mileageVO = new MileageVO();
-		mileageVO.setChangeMile(mileage);
-		mileageVO.setEnabledMile(mileage);
-		mileageVO.setMileContents(contents);
-		return mileageService.mileageForAll(mileageVO);
+	@GetMapping("selectedOrder")
+	public String selectedOrder(OrderlistVO orderlistVO)throws Exception{
+		Gson gson = new Gson();
+		return gson.toJson(orderService.selectedOrder(orderlistVO));
 	}
 	
-	//입력한 사람들에게만 마일리지를 전송
 	@ResponseBody
-	@PostMapping("mileageForSelect")
-	public Long mileageForSelect(Long mileage, String contents, String[] usernames)throws Exception{
-		List<MileageVO> list = new ArrayList<>();
-		for(String temp : usernames) {
-			MileageVO mileageVO = new MileageVO();
-			mileageVO.setUsername(temp);
-			mileageVO.setChangeMile(mileage);
-			mileageVO.setEnabledMile(mileage);
-			mileageVO.setMileContents(contents);
-			list.add(mileageVO);
+	@GetMapping("orderStateChange")
+	public String[] orderStateChange(OrderlistVO orderlistVO)throws Exception{
+		List<OrderlistVO> list = orderService.orderStateChange(orderlistVO);
+		Gson gson = new Gson();
+		int size = list.size();
+		String[] result = new String[size];
+		for(int i = 0 ; i < size; i++) {
+			result[i]=gson.toJson(list.get(i));
 		}
-		return mileageService.mileageForSelect(list);
+		return result;
 	}
 	
 	//하루 일치의 판매량을 집계하여 보여주는 페이지
@@ -259,6 +225,108 @@ public class AdminController {
 		return result;
 	}
 	
+	//쿠폰 종류를 입력하는 페이지로 이동
+	@GetMapping("setCouponsp")
+	public void setCouponsp()throws Exception {	}
+	
+	//쿠폰 종류 입력후 저장을 위함
+	@PostMapping("setCouponsp")
+	public String setCouponsp(CouponspVO couponspVO)throws Exception{
+		couponService.setCouponsp(couponspVO);
+		return "redirect:/admin/adminHome";
+	}
+	
+	//쿠폰 목록을 보여주는 페이지
+	@GetMapping("transmitCoupon")
+	public void transmitCoupon(Model model)throws Exception {
+		model.addAttribute("cuspList", couponService.getCouponspList());
+	}
+	
+	//선택한 쿠폰을 보낼 페이지로 이동
+	@GetMapping("selectedCoupon")
+	public void selectedCoupon(CouponspVO couponspVO, Model model)throws Exception{
+		model.addAttribute("item", couponService.getCouponsp(couponspVO));
+	}
+	
+	//쿠폰을 모든 사람들에게 전송
+	@ResponseBody
+	@GetMapping("couponforall")
+	public Long couponforall(Long cuSpNum, Integer period)throws Exception{
+		Calendar calendar = Calendar.getInstance();
+		java.sql.Date date1 = new java.sql.Date(calendar.getTimeInMillis());
+		calendar.add(Calendar.MONTH, period);
+		java.sql.Date date2 = new java.sql.Date(calendar.getTimeInMillis());
+		if(date1.after(date2)||date1.equals(date2)) {
+			return 0L;
+		}
+		CouponVO couponVO = new CouponVO();
+		couponVO.setCuSpNum(cuSpNum);
+		couponVO.setPubDate(date1);
+		couponVO.setExDate(date2);
+
+		return couponService.couponForAll(couponVO);
+	}
+	
+	//쿠폰을 입력한 username들에게 보냄
+	@ResponseBody
+	@PostMapping("couponServeral")
+	public Long couponServeral(String[] usernames, Long cuSpNum, Integer period)throws Exception{
+		List<CouponVO> list = new ArrayList<>();
+		Calendar calendar = Calendar.getInstance();
+		java.sql.Date date1 = new java.sql.Date(calendar.getTimeInMillis());
+		calendar.add(Calendar.MONTH, period);
+		java.sql.Date date2 = new java.sql.Date(calendar.getTimeInMillis());
+		if(date1.after(date2)||date1.equals(date2)) {
+			return 0L;
+		}
+		for(String username : usernames) {
+			CouponVO couponVO = new CouponVO();
+			couponVO.setUsername(username);
+			couponVO.setCuSpNum(cuSpNum);
+			couponVO.setPubDate(date1);
+			couponVO.setExDate(date2);
+			list.add(couponVO);
+		}
+		return couponService.couponForSelected(list);
+	}
+	
+	//마일리지 전송하는 페이지
+	@GetMapping("transmitMile")
+	public void transmitMile() { }
+	
+	//모든 사람들에게 마일리지를 전송
+	@ResponseBody
+	@GetMapping("transAllMile")
+	public Long transAllMile(Long mileage, String contents)throws Exception {
+		MileageVO mileageVO = new MileageVO();
+		mileageVO.setChangeMile(mileage);
+		mileageVO.setEnabledMile(mileage);
+		mileageVO.setMileContents(contents);
+		return mileageService.mileageForAll(mileageVO);
+	}
+	
+	//입력한 사람들에게만 마일리지를 전송
+	@ResponseBody
+	@PostMapping("mileageForSelect")
+	public Long mileageForSelect(Long mileage, String contents, String[] usernames)throws Exception{
+		List<MileageVO> list = new ArrayList<>();
+		for(String temp : usernames) {
+			MileageVO mileageVO = new MileageVO();
+			mileageVO.setUsername(temp);
+			mileageVO.setChangeMile(mileage);
+			mileageVO.setEnabledMile(mileage);
+			mileageVO.setMileContents(contents);
+			list.add(mileageVO);
+		}
+		return mileageService.mileageForSelect(list);
+	}
+	
+	//이하 내용은 product관련
+	@GetMapping("adminAllList")
+	public void adminAllList(Model model)throws Exception{
+		model.addAttribute("allList", productService.setUpdate());
+	}
+	
 	@GetMapping("productInsert")
 	public void productInsert(Model model)throws Exception{
 		String[] divisions = productService.getDivisions();
@@ -268,73 +336,6 @@ public class AdminController {
 	
 	@GetMapping("productDivInsert")
 	public void productDivInsert()throws Exception{	}
-	
-	@GetMapping("orderlistUpdate")
-	public void orderlistUpdate()throws Exception{	}
-	
-	@ResponseBody
-	@GetMapping("orderStateChange")
-	public String[] orderStateChange(OrderlistVO orderlistVO)throws Exception{
-		List<OrderlistVO> list = orderService.orderStateChange(orderlistVO);
-		Gson gson = new Gson();
-		int size = list.size();
-		String[] result = new String[size];
-		for(int i = 0 ; i < size; i++) {
-			result[i]=gson.toJson(list.get(i));
-		}
-		return result;
-	}
-	
-	@ResponseBody
-	@GetMapping("selectedOrder")
-	public String selectedOrder(OrderlistVO orderlistVO)throws Exception{
-		Gson gson = new Gson();
-		return gson.toJson(orderService.selectedOrder(orderlistVO));
-	}
-	
-	@ResponseBody
-	@PostMapping("orderlistUpdate")
-	@Transactional(rollbackFor = Exception.class)
-	public Long orderlistUpdate(OrderlistVO orderlistVO)throws Exception{
-		java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
-		Long temp = orderlistVO.getOrderState();
-		if(orderlistVO.getOrderState()==4) {
-			orderlistVO = orderService.selectedOrder(orderlistVO);
-			if(!date.toString().equals(orderlistVO.getOrderDate().toString())) {
-				List<PurchaseVO> list = purchaseService.getPurAdmin(orderlistVO);
-				if(adminService.cancledOrder(list) == 0) {
-					throw new Exception();
-				}
-			}
-		}else {
-			orderlistVO = orderService.selectedOrder(orderlistVO);
-			if(orderlistVO.getOrderState()==4) {
-				List<PurchaseVO> list = purchaseService.getPurAdmin(orderlistVO);
-				if(adminService.reCancledOrder(list) == 0) {
-					throw new Exception();
-				}
-			}
-		}
-		orderlistVO.setOrderState(temp);
-		return orderService.orderlistUpdate(orderlistVO);
-	}
-	
-	@ResponseBody
-	@PostMapping("orderlistsUpdate")
-	public Long orderlistsUpdate(String[] orderNums)throws Exception{
-		List<OrderlistVO> list = new ArrayList<OrderlistVO>();
-		for(String temp : orderNums) {
-			OrderlistVO orderlistVO = new OrderlistVO();
-			orderlistVO.setOrderNum(temp);
-			list.add(orderlistVO);
-		}
-		return orderService.orderlistsUpdate(list);
-	}
-	
-	@GetMapping("adminAllList")
-	public void adminAllList(Model model)throws Exception{
-		model.addAttribute("allList", productService.setUpdate());
-	}
 	
 	@GetMapping("productUpdate")
 	public void productUpdate(ProductVO productVO, Model model)throws Exception{
